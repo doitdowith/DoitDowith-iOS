89854799746d551ae9592f1b2e19fff8e9d46cbb
erimii <990303amy88@gmail.com>
2022-12-07 21:12:31 +0900

    달력부분 1차 완료

---
+++ DoitDowith-iOS/Sources/Presenter/Calendar/CustomCell.swift
@@ -0,0 +1,48 @@
+//
+//  CustomCell.swift
+//  DoitDowith-iOS
+//
+//  Created by 이예림 on 2022/12/07.
+//
+
+import Foundation
+import FSCalendar
+
+class CustomCell: FSCalendarCell {
+    static let identifier: String = "CustomCell"
+    lazy var stackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .vertical
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }();
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addSubview(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        stackView.topAnchor.constraint(equalTo: self.titleLabel.bottomAnchor, constant: 15).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+    
+    required init!(coder aDecoder: NSCoder!) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension CustomCell {
+    func configure(model: [String]) {
+        let colors = [UIColor(r: 255, g: 226, b: 226), UIColor(r: 227, g: 245, b: 213), UIColor(r: 25, g: 226, b: 226)]
+        for (index, event) in model.enumerated() {
+            let label = UILabel()
+            label.text = event
+            label.backgroundColor = colors[index]
+            self.stackView.addArrangedSubview(label)
+        }
+    }
+}
+
